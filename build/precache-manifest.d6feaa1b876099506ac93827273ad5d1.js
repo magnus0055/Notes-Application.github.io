dcd38cdf7821857f8ee980b94863990363822a0f
Satyajeet <www.satyajeet055@gmail.com>
2022-06-23 20:09:23 +0530

    second commit

---
+++ build/precache-manifest.d6feaa1b876099506ac93827273ad5d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363b6bfd93b07234d07a587b9c75ae55",
+    "url": "/Notes-Application.github.io/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Notes-Application.github.io/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "707570e2e8fe2d260b0b",
+    "url": "/Notes-Application.github.io/static/js/main.58c7ea08.chunk.js"
+  },
+  {
+    "revision": "ab711e7f67429f322c6c",
+    "url": "/Notes-Application.github.io/static/js/runtime-main.8c422059.js"
+  }
+]);
